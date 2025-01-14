966eccb7a77b4a6ca0b85739a6d5b055f5285cde
md kaif <mdkaifwtca@gmail.com>
2025-01-14 22:03:45 +0530

    add responsive

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+// Hamburger toggle functionality
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
